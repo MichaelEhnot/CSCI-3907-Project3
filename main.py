4c59a15665eed6be3d107696f75da3652d452f97
MichaelEhnot <mehnot@gwu.edu>
2022-04-05 20:10:23 -0400

    working on big matrix

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-from skimage import io
+from skimage import io, transform
 from matplotlib import pyplot as plt
 import numpy as np
 
-# name of the input file
-imname = 'PTZImages/image1.jpeg'
-
-# read in the image
-im = plt.imread(imname)
-
-# select the points
-plt.imshow(im)
-x = plt.ginput(4)
-print(x)
-
-
-
-
 def computeH(s, d):
-    # 4 points
+    # 5 points
     known_matrix = np.array([
         [s[0][0], s[0][1], 1, 0, 0, 0, (-d[0][0]*s[0][0]), (-d[0][0]*s[0][1]), -d[0][0]],
         [0, 0, 0, s[0][0], s[0][1], 1, (-d[0][1]*s[0][0]), (-d[0][1]*s[0][1]), d[0][1]],
@@ -30,8 +16,42 @@ def computeH(s, d):
 
         [s[3][0], s[3][1], 1, 0, 0, 0, (-d[3][0]*s[3][0]), (-d[3][0]*s[3][1]), -d[3][0]],
         [0, 0, 0, s[3][0], s[3][1], 1, (-d[3][1]*s[3][0]), (-d[3][1]*s[3][1]), d[3][1]],
+
+        [s[4][0], s[4][1], 1, 0, 0, 0, (-d[4][0]*s[4][0]), (-d[4][0]*s[4][1]), -d[4][0]],
+        [0, 0, 0, s[4][0], s[4][1], 1, (-d[4][1]*s[4][0]), (-d[4][1]*s[4][1]), d[4][1]]
     ])
 
-    zero_matrix = np.array([0,0,0,0,0,0,0,0,0])
+    zero_matrix = np.array([0, 0, 0, 0, 0, 0, 0, 0])
 
-    return np.linalg.solve(known_matrix, zero_matrix)
+    h = np.linalg.lstsq(known_matrix, zero_matrix)[0]
+    print("HELLO")
+    print(h)
+    H = np.array([
+        [h[0], h[1], h[2]],
+        [h[3], h[4], h[5]],
+        [h[6], h[7], 1]
+    ])
+
+    return H
+
+def warpImage(im, H):
+    tform = transform.ProjectiveTransform(matrix=H)
+    tform_im = transform.warp(im, tform.inverse)
+    return tform_im
+
+# read in the image
+im1 = plt.imread('PTZImages/image1.jpeg')
+im2 = plt.imread('PTZImages/image2.jpeg')
+
+# select the points
+plt.imshow(im1)
+im1_pts = plt.ginput(5)
+plt.imshow(im2)
+im2_pts = plt.ginput(5)
+
+H = computeH(im1_pts, im2_pts)
+print(H)
+
+imwarped = warpImage(im2, H)
+
+plt.imshow(imwarped)
